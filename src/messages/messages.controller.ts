4e1f008ab769c1a862c66a0ab2a497bbf7bf9bc5
Leonardo Silva <leonardosilvaif@hotmail.com>
2024-06-30 13:44:21 -0300

    Handling error when message id does not exist

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -24,8 +24,9 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    // console.log(id);
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) throw new NotFoundException('message not found');
+    return message;
   }
 }
